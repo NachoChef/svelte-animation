9b735c5d12436fa3bf05eb55fbd30ba3776230ee
Justin Jones <hock3ydud3@gmail.com>
2023-01-09 22:47:43 -0600

    up through lesson 5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,31 @@
 <script>
+	import { fade, blur, slide, fly } from 'svelte/transition';
 	import '$lib/app.css';
+
+	let ready = false;
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- Fade - opacity 0-1 -->
+<!-- Blur - opacity and blur -->
+<!-- Fly - x and y along with opacity -->
+<!-- Slide - height (basically an accordion) -->
+<!-- Scale - start: scale, opacity -->
+<!-- Draw - draw svgs -->
+
+<!-- css transition animation -->
+<!-- <h1 class:hidden={!ready}>Welcome to SvelteKit</h1> -->
+
+<!-- mounting and unmounting -->
+<button on:click={() => (ready = !ready)}>Toggle animation</button>
+
+{#if ready}
+	<!-- <h1 transition:slide>Welcome to SvelteKit</h1> -->
+	<h1 in:fly={{ y: 100 }} out:fade>Welcome to SvelteKit</h1>
+{/if}
+
+<style>
+	.hidden {
+		opacity: 0;
+		transform: 0.3s ease opacity;
+	}
+</style>
