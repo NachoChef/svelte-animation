88adec4ca8d6d4d064d5871026d0c0fb1ee29025
Justin Jones <hock3ydud3@gmail.com>
2023-01-10 20:27:55 -0600

    #11 but with 2d movement

--- src/lib/drag.js
+++ src/lib/drag.js
@@ -1,5 +1,57 @@
+import { spring } from 'svelte/motion';
+
 export function drag(node, params) {
-	node.addEventListener('mousedown', () => console.log('clicks'));
+	let x;
+	let y;
+
+	const coordinates = spring(
+		{ x: 0, y: 0 },
+		{
+			stiffness: 0.2,
+			damping: 0.4
+		}
+	);
+
+	coordinates.subscribe(($coords) => {
+		// in css, animations are most performant via transforms
+		// translate3d is more performant than translate, uses the gpu
+		node.style.transform = `translate3d(${$coords.x}px, ${$coords.y}px, 0)`;
+	});
+
+	node.addEventListener('mousedown', handleMouseDown);
+
+	function handleMouseDown(event) {
+		x = event.clientX;
+		y = event.clientY;
+		window.addEventListener('mousemove', handleMouseMove);
+		window.addEventListener('mouseup', handleMouseUp);
+	}
+
+	function handleMouseMove(event) {
+		// delta X -> distance from where we started to where we are
+		const dx = event.clientX - x;
+		const dy = event.clientY - y;
+		x = event.clientX;
+		y = event.clientY;
+		coordinates.update(($coords) => {
+			return {
+				x: $coords.x + dx,
+				y: $coords.y + dy
+			};
+		});
+	}
+
+	function handleMouseUp() {
+		window.removeEventListener('mousemove', handleMouseMove);
+		window.removeEventListener('mouseup', handleMouseUp);
+		coordinates.update(() => {
+			return {
+				x: 0,
+				y: 0
+			};
+		});
+	}
+
 	return {
 		destroy() {
 			node.removeEventListener('mousedown', () => console.log('click'));
