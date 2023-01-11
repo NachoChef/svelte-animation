d75180a2122d0de00ab77a292c2193f2bd8e0866
Justin Jones <hock3ydud3@gmail.com>
2023-01-10 20:00:41 -0600

    up through 10

---
+++ src/lib/Box.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { drag } from './drag';
+</script>
+
+<div class="box" use:drag />
+
+<style>
+	.box {
+		background: var(--primary);
+		box-shadow: var(--level-2);
+		height: 100px;
+		width: 100px;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,15 @@
 	import { fade, blur, slide, fly } from 'svelte/transition';
 	import '$lib/app.css';
 	import { custom } from './custom';
+	import Nav from '$lib/Nav.svelte';
+	import Box from '$lib/Box.svelte';
 
 	let ready = false;
+	let isNavOpen = false;
+
+	function toggleNav() {
+		isNavOpen = !isNavOpen;
+	}
 </script>
 
 <!-- Fade - opacity 0-1 -->
@@ -17,11 +24,19 @@
 <!-- <h1 class:hidden={!ready}>Welcome to SvelteKit</h1> -->
 
 <!-- mounting and unmounting -->
-<button on:click={() => (ready = !ready)}>Toggle animation</button>
+
+<button on:click={toggleNav}>Menu</button>
+
+{#if isNavOpen}
+	<Nav {toggleNav} />
+{/if}
+<!-- <button on:click={() => (ready = !ready)}>Toggle animation</button> -->
+
+<Box />
 
 {#if ready}
 	<!-- <h1 transition:slide>Welcome to SvelteKit</h1> -->
-	<h1 transition:custom={{ delay: 2000 }}>Welcome to SvelteKit</h1>
+	<h1 transition:custom>Welcome to SvelteKit</h1>
 {/if}
 
 <style>
